0d81ec2227e421a2b7e6e341b4ddd8beaa5b5dcc
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-21 19:59:12 +0530

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import Review from './Review';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h2>reviews project setup</h2>
     </div>
   );
 }
